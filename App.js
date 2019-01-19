6b8d8c4d7e8596d92bbe04347f46bd6ee55556c1
mish <mich0202@gmail.com>
2019-01-18 23:09:10 -0500

    Text change

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+//import { RNCamera, FaceDetector } from 'react-native-camera';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome to our project :-)</Text>
       </View>
     );
   }
