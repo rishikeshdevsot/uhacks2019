b8332b061910fb555035ffa11cf93f133f6a11c0
rishikeshdevsot <rishikeshdevsot@gmail.com>
2019-01-19 22:34:16 -0500

    Added text classification

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+
 import React, { Component } from 'react';
-import { Button, Text, ScrollView, StyleSheet } from 'react-native';
+import { Button, Text, ScrollView, StyleSheet, View } from 'react-native';
 import { ImagePicker, Permissions, Constants } from 'expo';
+
 
 
 export default class App extends Component {
@@ -30,6 +32,10 @@ export default class App extends Component {
     features:[{"type":"TEXT_DETECTION","maxResults":10},{"type":"DOCUMENT_TEXT_DETECTION","maxResults":10}],
     },],
     };
+
+    
+
+
       const response = await fetch("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCznKndZSAxTcAPBn8VuamKN2e9SLc5hQY", 
     {
     method: 'POST',
@@ -40,9 +46,18 @@ export default class App extends Component {
     body: JSON.stringify(body),
     });
 
+    var body2 = 
+    {
+          "document": {
+            type: "PLAIN_TEXT",
+            language:"EN",
+            content:"Lawrence of Arabia is a highly rated film biography about British Lieutenant T. E. Lawrence. Peter O'Toole plays Lawrence in the film.",
+          }
+    };
+
       const json = await response.json();
       //console.log(json);
-      console.log(json.responses[0].fullTextAnnotation.text);
+      //console.log(json.responses[0].fullTextAnnotation.text);
       //console.log(json.responses[0].fullTextAnnotation.pages[0].blocks[0].boundingBox.vertices[0].y);
       //console.log(json.responses[0].fullTextAnnotation.pages[0].blocks[0].text);
       //console.log("-----------------------------------------------------------------------------");
@@ -56,8 +71,63 @@ export default class App extends Component {
     const myArrStr3 = JSON.parse(myArrStr2);
     console.log(myArrStr3{responses});
     */
+    var s = '';
+    var z = '';
+    var i =0;
+    var json2;
+    //for (let i=0; i < json.responses[0].fullTextAnnotation.pages.length; i++){
+      for(let j=0; j< json.responses[0].fullTextAnnotation.pages[i].blocks.length; j++){
+        for(let k=0; k<json.responses[0].fullTextAnnotation.pages[i].blocks[j].paragraphs.length; k++){
+          for(let l=0; l<json.responses[0].fullTextAnnotation.pages[i].blocks[j].paragraphs[k].words.length; l++){
+            for(let m=0; m<json.responses[0].fullTextAnnotation.pages[i].blocks[j].paragraphs[k].words[l].symbols.length; m++){
+                
+              z = z + json.responses[0].fullTextAnnotation.pages[i].blocks[j].paragraphs[k].words[l].symbols[m].text;
+              //console.log(json.responses[0].fullTextAnnotation.pages[i].blocks[j].paragraphs[k].words[l].symbols[m].text);
+                
+            }
+            //s = s + ' ';
+            z = z + ' ';
+          }
+        }
+        var h = z.split(" ");
+        //console.log(h);
+        if(h.length >= 20){
+          body2.document.content = z;
+          //console.log(body2);
 
-  };
+          var response2 = await fetch("https://language.googleapis.com/v1/documents:classifyText?key=AIzaSyCbgPPNl5-CA9qw5ND0hcqDis1XEWeC61Y", 
+          {
+              method: 'POST',
+              headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body2),
+          });
+
+          json2 = await response2.json();
+          if(json2.categories!=undefined){
+            console.log(z + ' -- ');
+            console.log(json2.categories);
+            console.log('\n');
+          }
+          
+          //console.log(z);
+          //console.log(json2);
+          //console.log(json2.categories[0].name);
+          //s = s + z + ' - ' + json2.categories[0].name + '\n';
+          z = z + ' - ' + json2 + '\n';
+          s = s + z;
+          //console.log(z);
+          z = '';
+        }
+        else{
+          console.log('--------');
+        }
+      }
+    //}
+    //console.log(s);
+};
 
   useCameraHandler = async () => {
     await this.askPermissionsAsync();
@@ -87,11 +157,9 @@ export default class App extends Component {
   this.setState({result: response});
   const myArrStr = JSON.stringify(response);
 
-  console.log(JSON.parse(myArrStr));
+  //console.log(JSON.parse(myArrStr));
 
   };
-
-
 
   render() {
     return (
